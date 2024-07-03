9d8a6f5acb79e28fc6b3ac77358b5808dfdaabbd
guibaumer <117869143+guibaumer@users.noreply.github.com>
2024-07-03 19:28:09 -0300

    finalizing2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class App {
     this.app.use((req, res, next) => {
       // res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3001']); 
       // res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3001']); 
-      res.setHeader('Access-Control-Allow-Origin', ['https://pombal-registros.netlify.app/']); 
+      res.setHeader('Access-Control-Allow-Origin', ['https://pombal-registros.netlify.app']); 
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
       res.setHeader('Access-Control-Allow-Headers', 'content-type');
